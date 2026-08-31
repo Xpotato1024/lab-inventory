38ca9619aa4d005e830d32301ffdbae8a7390c4c
Xpotato1024 <widwmitsui@gmail.com>
2026-08-31 18:02:29 +0900

    feat: add low-stock status view

---
+++ inventory/status_views.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+from .status import low_stock_items
+
+
+@login_required
+def low_stock(request):
+    items = list(low_stock_items())
+    return render(
+        request,
+        "inventory/low_stock.html",
+        {"items": items},
+    )
