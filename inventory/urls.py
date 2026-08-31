d3dc72013d3c4898ea89bf5bee9d987f7dac87fe
Xpotato1024 <widwmitsui@gmail.com>
2026-08-31 17:40:05 +0900

    feat: route procedural 3D viewer

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from . import bulk_views, views
+from . import bulk_views, scene_views, views
 
 app_name = "inventory"
 
 urlpatterns = [
     path("healthz/", views.healthz, name="healthz"),
     path("", views.home, name="home"),
+    path("3d/", scene_views.room_3d, name="room-3d"),
     path("i/<str:code>/", views.item_detail, name="item-detail"),
     path("u/<str:code>/", views.unit_detail, name="unit-detail"),
     path("u/<str:code>/move/", views.unit_move, name="unit-move"),
